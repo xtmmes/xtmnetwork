066fd248c44b84295eb7c5fe124e7ec9d588ab8a
Евгений Лях <xtm791@gmail.com>
2022-05-18 18:05:09 +0300

    на ревью 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -109,14 +109,24 @@ def follow_index(request):
 
 @login_required
 def profile_follow(request, username):
-    author = get_object_or_404(User, username=username)
+    author = get_object_or_404(
+        User,
+        username=username
+    )
     if request.user != author:
-        Follow.objects.get_or_create(user=request.user, author=author)
+        Follow.objects.get_or_create(
+            user=request.user,
+            author=author
+        )
     return redirect("posts:profile", username=username)
 
 
 @login_required
 def profile_unfollow(request, username):
     """Функция отписаться от некого автора"""
-    Follow.objects.filter(user=request.user, author__username=username).delete()
+    Follow.objects.filter(
+        user=request.user,
+        author__username=username
+    ).delete()
     return redirect('posts:profile', username)
+
